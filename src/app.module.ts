54d17a912e7ab15b2ef83f1c7c6e2527c5ee0c09
nndafnmain <nndafnmain@gmail.com>
2025-01-09 17:43:32 +0700

    prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { UsersModule } from "./modules/users/users.module";
 import { AuthModule } from "./modules/auth/auth.module";
 import { ProductModule } from "./modules/product/product.module";
+import { R2Module } from './modules/r2/r2.module';
 
 @Module({
-	imports: [UsersModule, AuthModule, ProductModule],
+	imports: [UsersModule, AuthModule, ProductModule, R2Module],
 	controllers: [],
 	providers: [],
 })
--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from "@nestjs/common";
+import { R2Service } from "../r2/r2.service";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Controller("product")
-export class ProductController {}
+export class ProductController {
+	constructor(
+		private readonly r2Service: R2Service,
+		private readonly prisma: PrismaService,
+	) {}
+}
